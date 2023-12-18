cc14ec688a857c1bc934c0015176b218fadb26ee
muhammet ali petek <malipetek@gmail.com>
2023-12-18 22:59:00 +0300

    switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,16 +6,18 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-    postcss: true
+		postcss: true
 	}),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			out: 'build'
+		})
 	},
 	vitePlugin: {
 		inspector: false
 	},
 	onwarn: (warning, handler) => {
-		if(warning.filename.includes('svelte-json-tree')) {return}
+		if (warning.filename.includes('svelte-json-tree')) { return; }
 		handler(warning);
 	}
 };
